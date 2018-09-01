12c3ce8eeffccc956b4b8a7ad7677b755987ac55
Bruno Rodrigues <bruno@urbano.eti.br>
2018-08-31 21:42:25 -0300

    * add debugactivity class

--- app/src/main/java/br/eti/urbano/mobile/force4sale/activities/HomeActivity.java
+++ app/src/main/java/br/eti/urbano/mobile/force4sale/activities/HomeActivity.java
@@ -2,14 +2,24 @@ package br.eti.urbano.mobile.force4sale.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import br.eti.urbano.mobile.force4sale.R;
 
-public class HomeActivity extends AppCompatActivity {
+public abstract class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void abrirTela(View view) {
+
+        switch (view.getId()){
+            default:
+
+        }
     }
 }
